d27cad474ca9dcc1280f5bac7e3573c29882eced
natalietwitchell <natalie.twitchell@gmail.com>
2017-03-28 18:17:19 -0400

    Create USERID_H.h

---
+++ USERID_H.h
@@ -0,0 +1,47 @@
+#ifndef USERID_H
+#define USERID_H
+
+using namespace std; 
+
+//enum userColor { #800000, #808000, #00FFFF, #008080, #FF00FF, 
+		//#800080, #FFFF00, #FFFFFF, #FF0000, #00FF00 };
+class userID {
+
+public: 
+
+
+//Constructor
+userID(); 
+userID(userTime, userColor, userName); 
+
+//Get the userTime
+unsigned int getUserTime() const;
+
+//set userTime
+
+void setTime(int timeOfLoad); 
+
+//Get userColor
+string getUserColor() const; 
+
+//set userColor
+
+void setUserColor(string color);
+
+//Get userName
+string getUserName() const; 
+
+//set username
+
+void setUserName(string username);
+
+
+private: 
+
+userTime = timeOfLoad
+userColor = color;
+userName = username; 
+
+};
+
+#endif /* USERID_H */
