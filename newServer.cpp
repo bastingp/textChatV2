5ea7a760260338c1d5ed579046e731450ed1c5fd
natalietwitchell <natalie.twitchell@gmail.com>
2017-04-02 17:17:13 -0400

    ParseIncomingData function drafted

--- newServer.cpp
+++ newServer.cpp
@@ -37,9 +37,107 @@ struct IncomingData
 
 vector<User> activeUsers;			//all users signed into the server
 
+void ParseIncomingData(IncomingData incomingData, string message);
+
+
 int main()
 {
 	IncomingData incomingData;
 	
+	string message = "dummymessage";
+	
+	ParseIncomingData(incomingData, message);
+	
+	
 	return 0;
 }
+
+
+ParseIncomingData(IncomingData incomingData, string message){
+	int i = 0 //index; 
+	
+	//get the timecode
+	while (i < message.size())
+	{
+		if (message[i] == '$')
+		{
+			break;
+		}
+		index++
+	}
+	index++
+	
+	while(i < message.size())
+	{
+		if(message[i] == '|')
+			{
+				break;
+			}
+			
+			incomingData.timecode += message[i];
+			index++; 
+	}
+	index++; 
+	
+	//figure out what's next; 
+	
+	if (message[i] == '$'){ //get command
+	while(i < message.size()) 
+	{
+		if(message[i] == '|')
+			{
+				break;
+			}
+			
+			incomingData.command += message[i];
+			index++; 
+	}
+	index++; 
+	}
+	
+	//get everything else; 
+	
+	//get Username
+	while(i < message.size()) 
+	{
+		if(message[i] == '|')
+			{
+				break;
+			}
+			
+			incomingData.username += message[i];
+			index++; 
+	}
+	index++; 
+
+
+	//get Message
+	while(i < message.size()) 
+	{
+		if(message[i] == '|')
+			{
+				break;
+			}
+			
+			incomingData.message += message[i];
+			index++; 
+	}
+	index++; 
+	
+	
+	//get Message size
+	 
+	while(i < message.size()) 
+	{
+		if(message[i] == '|')
+			{
+				break;
+			}
+			
+			incomingData.messageSize += message[i];
+			index++; 
+	}
+	index++; 
+	}
+	
+		
