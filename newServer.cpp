a2b1af82a1e52096dee57c079b1cfba9a3d01bff
Joey Schutz <schutzj@kenyon.edu>
2017-04-02 16:57:56 -0400

    made IncomingData struct

--- newServer.cpp
+++ newServer.cpp
@@ -1,1 +1,35 @@
 //Server created on April 2
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+#include "fifo.h"
+#include "USER_H.h"
+
+using namespace std;
+
+//used to store data from cgi
+struct IncomingData
+{
+	string timeCode = "";
+	string command = "";
+	string username = "";
+	string message = "";
+	string userMessageSize = "";
+};
+
+vector<User> activeUsers;			//all users signed into the server
+
+int main()
+{
+	IncomingData incomingData;
+	
+	return 0;
+}
