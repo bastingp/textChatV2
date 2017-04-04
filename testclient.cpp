1d4b45b905f5c8e3feabf5bc10cffc7140d32dca
Preston Basting <bastingp@kenyon.edu>
2017-04-04 17:20:17 -0400

    test client sends all message types

--- testclient.cpp
+++ testclient.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include "fifo.h"
 #include <time.h>
+#include <istream>
 
 using namespace std;
 string receive_fifo = "chatReply";
@@ -24,10 +25,14 @@ int main()
 	string load_message = "$" + to_string(t) + "|LOAD|*";
 	string unload_message = "$" + to_string(t) + "|UNLOAD|user1*";
 	string status_check_message = "$STATUS%*";
+
+	string end_messages = "$END";
+	string uptodate = "$UPTODATE";
 	string word;
 	string message;
 	string reply;
 	string username = "";
+	string received_messages="0";
 	
 	
 	
@@ -35,37 +40,110 @@ int main()
 	Fifo recfifo(receive_fifo);
 	Fifo sendfifo(send_fifo);
 
-	/////ASSIGNMENT TEST/////////
-	cout << "\n\nAbout to open send pipe\n\n";
-	sendfifo.openwrite();
-	cout << "\n\nSuccess opening send pipe\n\n";
-	
-	sendfifo.send(load_message);
-
-
-	cout << "About to open receive pipe";
-	recfifo.openread();
-	reply = recfifo.recv();
-	username = get_username(reply);
-	cout << "received from server: "<< reply << endl;
-	recfifo.fifoclose();
-	sendfifo.fifoclose();
-
-	cout << "asigned user:" << username <<endl;
-
-	cout << "Continue? (Type anything):";
-	cin.ignore();
-
-	/////////UNASSIGNMENT TEST////////////////
-
-
-	if(username != "FULL")
+	while(1)
 	{
-		sendfifo.openwrite();
-		sendfifo.send(unload_message);
-		cout << "sent to server:" << unload_message << endl;
 
-		sendfifo.fifoclose();
+		int testChoice;
+		cout << "(1)Load, (2)Message, (3)Update, (4)Unload" << endl;
+		cout <<"Action:";
+		cin >> testChoice;
+
+		switch(testChoice)
+		{
+			case 1: //Load test 
+			{
+			cout << "\n\nAbout to open send pipe\n\n";
+			sendfifo.openwrite();
+			cout << "\n\nSuccess opening send pipe\n\n";
+			
+			sendfifo.send(load_message);
+
+
+			cout << "About to open receive pipe";
+			recfifo.openread();
+			reply = recfifo.recv();
+			username = get_username(reply);
+			cout << "received from server: "<< reply << endl;
+			recfifo.fifoclose();
+			sendfifo.fifoclose();
+
+			cout << "asigned user:" << username <<endl;
+			break;
+			}
+
+			case 2: //Message test
+			{
+				cout << "Message:"<< endl;
+				//getline(cin, message);
+				cin >> message;
+				cout << "Number of messages received:";
+				cin >> received_messages;
+
+
+				cout << "\n\nAbout to open send pipe\n\n";
+				sendfifo.openwrite();
+				cout << "\n\nSuccess opening send pipe\n\n";
+				
+				string message_to_send = "$"+to_string(t)+"|MESSAGE|"+username+"|"+message+"|"+received_messages+"*";
+				cout << "sending to server:"<<message_to_send<<endl;
+				sendfifo.send(message_to_send);
+				cout << "\n\nOpening receive pipe...\n\n";
+				recfifo.openread();
+				reply = recfifo.recv();
+				cout << endl<< "Updating......." << endl;
+				while(reply.find(end_messages) > 0)
+				{
+					cout << reply << endl;
+					if(reply.find(uptodate) != string::npos)
+					{
+						break;
+					}
+					reply = recfifo.recv();
+				}
+
+				recfifo.fifoclose();
+				sendfifo.fifoclose();
+				break;
+			}
+
+			case 3: //Update test
+			{
+				sendfifo.openwrite();
+				cout << "Number of messages received:";
+				cin >> received_messages;
+				string update_message = "$" + to_string(t) + "|UPDATE|"+username+"|"+received_messages+"*";
+				cout << "sending:" << update_message;
+				sendfifo.send(update_message);
+				recfifo.openread();
+				cout << "receiving update..." << endl;
+				reply = recfifo.recv();
+				while(reply.find(end_messages) > 0)
+				{
+					cout << reply << endl;
+					if(reply.find(uptodate) != string::npos)
+					{
+						break;
+					}
+					reply = recfifo.recv();
+				}
+
+				recfifo.fifoclose();
+				sendfifo.fifoclose();
+				break;
+			}
+
+			case 4: //unload test
+			{
+				sendfifo.openwrite();
+				unload_message = "$" + to_string(t) + "|UNLOAD|" + username +"*";
+				sendfifo.send(unload_message);
+				cout <<"sent to server:" << unload_message;
+				sendfifo.fifoclose();
+				break;
+			}
+
+		}
+
 	}
 	
 	return 0;
