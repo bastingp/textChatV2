aa00bd647da48821baeeaeff207c676b25684d7c
natalietwitchell <natalie.twitchell@gmail.com>
2017-04-05 10:21:20 -0400

    comments

--- USER.cpp
+++ USER.cpp
@@ -14,7 +14,7 @@ User::User()
 	lastUpdateTime = 0;
 }
 
-User::User(string t, string c, string n) 
+User::User(string t, string c, string n) //initializes user;
 {
     timeCode = t; 
     color = c;
@@ -22,7 +22,7 @@ User::User(string t, string c, string n)
 	lastUpdateTime = 0;
 }
 
-User::User(string t , string c, string n, time_t ut)
+User::User(string t , string c, string n, time_t ut)//initializes user
 {
 	timeCode = t; 
     color = c;
@@ -30,27 +30,27 @@ User::User(string t , string c, string n, time_t ut)
 	lastUpdateTime = ut;
 }
 
-string User::GetTime() const 
+string User::GetTime() const //gets the timecode
 {
     return (timeCode);
 }
 
-string User::GetUsername() const 
+string User::GetUsername() const //gets the username
 {
     return (username);
 }
 
-string User::GetColor() const 
+string User::GetColor() const //gets the color
 {
     return (color);
 }
 
-time_t User::GetLastUpdateTime() const
+time_t User::GetLastUpdateTime() const //gets the last update time (seconds in time_t)
 {
 	return (lastUpdateTime);
 }
 
-void User::SetLastUpdateTime(time_t newUpdateTime)
+void User::SetLastUpdateTime(time_t newUpdateTime) //sets the last update time 
 {
 	lastUpdateTime = newUpdateTime; 
 }
