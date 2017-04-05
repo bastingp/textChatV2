553738beef1ed4a6b8e9a3916dcf8aa4e0fb7178
Joey Schutz <schutzj@kenyon.edu>
2017-04-04 20:47:32 -0400

    send load message with time code on page load

--- textchat.js
+++ textchat.js
@@ -1,6 +1,26 @@
 var XMLHttp;
+var timeCode;
 
 function pageInit() 
 {
 	console.log("Page load");
+	timeCode = Date.now();
+	
+	//setup XMLHttp
+	if(navigator.appName == "Microsoft Internet Explorer") 
+	{
+		XMLHttp = new ActiveXObject("Microsoft.XMLHTTP");
+	} else 
+	{
+		XMLHttp = new XMLHttpRequest();
+	}
+	
+	//send load message to CGI
+	callCGI("$" + timeCode "|LOAD|*");
 }
+
+function callCGI(CGIMessage)
+{
+	console.log("Sending to CGI: " + CGIMessage);
+}
+
