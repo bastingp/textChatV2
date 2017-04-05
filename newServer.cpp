f42bbd09e62d43028c839ce6bbb7e6bc37931055
Joey Schutz <schutzj@kenyon.edu>
2017-04-04 21:15:11 -0400

    changed update messages to preface it with ''

--- newServer.cpp
+++ newServer.cpp
@@ -322,6 +322,7 @@ vector<string> GetUpdateMessages(IncomingData data)
 	}
 	else if(data.userMessageSize < numMessages.str())
 	{
+		updateMessages.push_back("$UPDATE");
 		for(int i = atoi(data.userMessageSize.c_str()); i < storedMessages.size(); i++)
 		{
 			updateMessages.push_back(storedMessages[i]);
--- textchat.js
+++ textchat.js
@@ -52,3 +52,5 @@ function checkServerForUpdates()
 	callCGI("")
 }
 
+
+
